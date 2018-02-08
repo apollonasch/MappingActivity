5d5788dcdba37819be8b3e4ef98436c8506bb8f6
Apollonas <capollonas@yahoo.com>
2018-02-08 13:05:02 +0000

    c

--- app/src/main/java/com/example/apollonas/application/MainActivity.java
+++ app/src/main/java/com/example/apollonas/application/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.apollonas.application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
 
+public class FeetToMetresActivity extends AppCompatActivity implements OnClickListener {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv=new TextView(this);
-        tv.setText("Hello !");
         setContentView(R.layout.activity_main);
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+        TextView tv = (TextView)findViewById(R.id.tv1);
+        EditText et = (EditText)findViewById(R.id.et1);
+        double feet = Double.parseDouble(et.getText().toString());
+        double metres = feet*0.305;
+        tv.setText("In metres that is: " + metres);
     }
 }
